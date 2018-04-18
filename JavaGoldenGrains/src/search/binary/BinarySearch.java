7e82a175bde095249d3affcb55b1dce9f90f4ef2
Administrator <Administrator@10.24.64.30>
2018-04-18 11:00:55 +0200

    Added binary search example.

---
+++ JavaGoldenGrains/src/search/binary/BinarySearch.java
@@ -0,0 +1,35 @@
+package search.binary;
+
+public class BinarySearch {
+
+    public static int binarySearchArray(int[] arr, int target) {
+        boolean found = false;
+        int left = 0;
+        int right = arr.length - 1;
+        int middle = -1;
+        while (!found && left <= right) {
+            middle = (left + right) / 2;
+            int k = arr[middle];
+            if (k == target) {
+                found = true;
+            } else if (k > target) {
+                right = middle - 1;
+            } else {
+                left = middle + 1;
+            }
+        }
+
+        if (found) {
+            return middle;
+        } else {
+            return -1;
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] arr = { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15 };
+        int searchFor = 13;
+        System.out.println("Value " + searchFor + " is positioned at: " + binarySearchArray(arr, searchFor));
+    }
+
+}
